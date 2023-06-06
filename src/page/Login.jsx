898b3bd3d4c077a4b35c350d217143305976c619
Rania Gufaifia <128307988+rania-guefaifia872@users.noreply.github.com>
2023-06-06 22:10:45 +0200

    Update Login.jsx

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -17,11 +17,11 @@ const Login = () => {
     try {
       const response = await axios.post('/api/auth/connecter', { email, password });
       console.log(response.data);
-      // Redirect to "/accueil" after successful login
+      // Redirection vers "/accueil" aprés la connexion
       navigate('/compte');
     } catch (error) {
       console.log(error);
-      // Display error message received from the backend
+      // Afficher un message d'erreur venant du backend
       setError(error.response.data.msg);
     }
   };
@@ -29,31 +29,39 @@ const Login = () => {
   useEffect(() => {
     setIsSmallScreen(window.innerWidth < 670);
     window.addEventListener('resize', () => setIsSmallScreen(window.innerWidth < 670));
+    // Pour la version responsive de la page "/LogIn"
   }, []);
 
   return (
     <div className='flex flex-wrap'>
       <div className={`w-full md:w-1/3 p-0 ${isSmallScreen ? 'h-[150px]' : 'h-screen'} order-2 md:order-2 flex justify-center items-center md:items-start relative`}>
+  {/* Is small screen pour la version responsive de la page Login  */}
         <img className={`${isSmallScreen ? 'hidden' : ''}`} src={JouetEnfant} alt='JouetEnfant' />
       </div>
       <div className='w-full p-6 md:w-2/3 h-screen order-1 md:order-1 flex justify-center items-center lg:mt-[20px] mb-4'>
         <div className='flex flex-col items-center justify-center md:w-2/3 lg:w-2/3 mx-auto'>
           <img className='pb-14 w-[350px]' src={BlueShape} alt='MignonBleu' />
           <form className='w-full' onSubmit={handleSubmit}>
+          {/* Pour envoyez le formulaire de connexion */}
             <input className='block w-full border p-4 mb-5 rounded-lg bg-[#D9D9D9] active:text-[#000000] black-text' type='email' placeholder='E-mail ou numero de téléphone' value={email} onChange={(e) => setEmail(e.target.value)}></input>
             <input className='block w-full border p-4 mb-3 rounded-lg bg-[#D9D9D9] active:text-[#000000] black-text ' type='password' placeholder='Mot de passe' value={password} onChange={(e) => setPassword(e.target.value)}></input>
             <p>Mot de passe oublié ?</p>
+            {/* La gestion des erreurs */}
             <button className='bg-[#FFB1A6] text-[#094076] font-bold py-2 w-full rounded-3xl mt-6 mb-4' type='submit'>Se Connecter</button>
           </form>
           {error && (
+            
             <div className="error-box">
+            {/* Afficher un message d'erreur à l'utilisateur */}
               <p className="error-message">{error}</p>
             </div>
           )}
         </div>
       </div>
       <style>
+      {/* Le style des messages d'erreur */}
         {`
+        
         .error-box {
           background-color: #00000;
           color: #094076;
