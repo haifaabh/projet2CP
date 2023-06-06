628103a139120bf60de2a06a6e9c6507cc7f9ed0
Dina <102818956+Dinouch@users.noreply.github.com>
2023-06-06 22:13:37 +0100

    Add files via upload

--- src/page/HomePage.js
+++ src/page/HomePage.js
@@ -1,20 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'; // Importation de React et useState depuis la bibliothèque 'react'
 // import Texte from './Texte';
-import Recherche from './Recherche';
-import axios from 'axios';
+import Recherche from './Recherche'; // Importation du composant Recherche depuis './Recherche'
+import axios from 'axios'; // Importation d'axios pour effectuer des requêtes HTTP
 
 function HomePage() {
-  const [localisation, setLocalisation] = useState('');
-  const [creches, setCreches] = useState([]);
+  const [localisation, setLocalisation] = useState(''); // Variable d'état pour stocker la localisation
+  const [creches, setCreches] = useState([]); // Variable d'état pour stocker les résultats de recherche des crèches
 
   const handleLocalisationChange = (event) => {
-    setLocalisation(event.target.value);
+    setLocalisation(event.target.value); // Mise à jour de la localisation avec la valeur saisie par l'utilisateur
   };
 
   const handleSearch = async () => {
     try {
-      const response = await axios.post('/api/invite/rechercher-lieu', { localisation });
-      setCreches(response.data);
+      const response = await axios.post('/api/invite/rechercher-lieu', { localisation }); // Envoi d'une requête POST à l'API backend pour rechercher des crèches en fonction de la localisation
+      setCreches(response.data); // Mise à jour des résultats de recherche avec les données récupérées
     } catch (error) {
       console.log('yooo');
       console.error(error);
@@ -39,4 +39,4 @@ function HomePage() {
   );
 }
 
-export default HomePage;
+export default HomePage; // Exportation du composant HomePage
