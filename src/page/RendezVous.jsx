f075460f14bc25d7b65bd769a13adbbe7984a085
Rania Gufaifia <128307988+rania-guefaifia872@users.noreply.github.com>
2023-06-06 22:19:38 +0200

    Update RendezVous.jsx

--- src/page/RendezVous.jsx
+++ src/page/RendezVous.jsx
@@ -7,14 +7,14 @@ import { DayCalendarSkeleton } from '@mui/x-date-pickers/DayCalendarSkeleton';
 import { TimeField } from '@mui/x-date-pickers/TimeField';
 import axios from 'axios';
 
-const RendezVous = ({ onPlusDinfoclick, onReserverClick,onAnnulerClick ,id}) => {
-  const initialValue = dayjs('2023-04-17');
-  const [isLoading, setIsLoading] = React.useState(false);
-  const [statusMessage, setStatusMessage] = React.useState('');
-  const [value, setValue] = React.useState(dayjs('2022-04-17T15:30'));
-  const [value1, setValue1] = React.useState(dayjs('2022-04-17T15:30'));
-  const [selectedDate, setSelectedDate] = useState(initialValue);
-  const [isSmallScreen, setIsSmallScreen] = useState(false);
+const RendezVous = ({ onPlusDinfoclick, onReserverClick, onAnnulerClick, id }) => {
+  const initialValue = dayjs('2023-04-17'); // Valeur initiale pour la date sélectionnée
+  const [isLoading, setIsLoading] = React.useState(false); // État de chargement
+  const [statusMessage, setStatusMessage] = React.useState(''); // Message d'état
+  const [value, setValue] = React.useState(dayjs('2022-04-17T15:30')); // Heure de fin du rendez-vous
+  const [value1, setValue1] = React.useState(dayjs('2022-04-17T15:30')); // Heure de début du rendez-vous
+  const [selectedDate, setSelectedDate] = useState(initialValue); // Date sélectionnée
+  const [isSmallScreen, setIsSmallScreen] = useState(false); // État pour écran de petite taille
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
@@ -22,9 +22,9 @@ const RendezVous = ({ onPlusDinfoclick, onReserverClick,onAnnulerClick ,id}) => 
 
   const handleConfirmClick = async () => {
     const rdvData = {
-      date: selectedDate.toISOString().substr(0, 10),
-      heure_debut: value1.toISOString().substr(11, 5),
-      heure_fin: value.toISOString().substr(11, 5),
+      date: selectedDate.toISOString().substr(0, 10), // Date au format ISO
+      heure_debut: value1.toISOString().substr(11, 5), // Heure de début au format ISO
+      heure_fin: value.toISOString().substr(11, 5), // Heure de fin au format ISO
     };
     try {
       const response = await axios.post(`/api/parent/prendre_rdv/${id}`, rdvData, {
@@ -38,69 +38,66 @@ const RendezVous = ({ onPlusDinfoclick, onReserverClick,onAnnulerClick ,id}) => 
   };
 
   useEffect(() => {
-    setIsSmallScreen(window.innerWidth < 670);
-    window.addEventListener("resize", () => setIsSmallScreen(window.innerWidth < 670));
+    setIsSmallScreen(window.innerWidth < 670); // Vérifier si l'écran est de petite taille lors du chargement initial
+    window.addEventListener('resize', () => setIsSmallScreen(window.innerWidth < 670)); // Mettre à jour l'état lorsque la taille de l'écran change
   }, []);
 
   return (
     <div className={`relative flex justify-center  top-4`}>
-    <div className={`absolute  h-12 w-[80%] z-10 rounded-xl bg-[#FFB1A6] flex justify-end  ${isSmallScreen ? '' : 'mt-6'}`}>
-      <button className='mr-3 ml-4 font-semibold text-white' onClick={onPlusDinfoclick}>Plus d'infos</button>
-      <button className='mr-4 ml-3 font-semibold text-white' onClick={onReserverClick}>Reserver</button>
-    </div>
-    <div className={`absolute ${isSmallScreen ? 'w-[65%]' : 'w-[150vh]'} absolute flex flex-col sm:flex-row justify-center`}>
-      <div className={` ${isSmallScreen ? ' w-[100%] mt-[20%]' : 'w-[35%] mt-20 mr-[15vh]'} mb-1 `}>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DateCalendar
-            value={selectedDate}
-            onChange={handleDateChange}
-            loading={isLoading}
-            renderLoading={() => <DayCalendarSkeleton />}
-            className={`${isSmallScreen ? 'w-full' : 'w-[50vh]'}`}
-      style={{ width: isSmallScreen ? '100vh' : '100vh', maxWidth: '100%', height: isSmallScreen ? '40%' : '100vh' }}
-
-          />
-        </LocalizationProvider>
-        <input
-          type="text"
-          value={selectedDate.format('YYYY-MM-DD')}
-          className={` sm:my-0 mx-2 border-2 border-gray-200 rounded-xl px-4 py-2 my-4 ${isSmallScreen ? 'w-[90%] ' : 'w-[80%]  ml-5'}`}
-          readOnly
-        />  
+      <div className={`absolute  h-12 w-[80%] z-10 rounded-xl bg-[#FFB1A6] flex justify-end  ${isSmallScreen ? '' : 'mt-6'}`}>
+        <button className='mr-3 ml-4 font-semibold text-white' onClick={onPlusDinfoclick}>Plus d'infos</button>
+        <button className='mr-4 ml-3 font-semibold text-white' onClick={onReserverClick}>Reserver</button>
       </div>
-      <div className={`flex justify-center ${isSmallScreen ? ' mt-5' : 'flex-col  mt-[20vh]'}`} >
-        <div className={` w-[100%]`}>
+      <div className={`absolute ${isSmallScreen ? 'w-[65%]' : 'w-[150vh]'} absolute flex flex-col sm:flex-row justify-center`}>
+        <div className={` ${isSmallScreen ? ' w-[100%] mt-[20%]' : 'w-[35%] mt-20 mr-[15vh]'} mb-1 `}>
           <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <TimeField
-              label="Heure Debut rendez-vous"
-              value={value1}
-              onChange={(newValue) => setValue1(newValue)}
-              format="HH:mm"
+            <DateCalendar
+              value={selectedDate}
+              onChange={handleDateChange}
+              loading={isLoading}
+              renderLoading={() => <DayCalendarSkeleton />}
+              className={`${isSmallScreen ? 'w-full' : 'w-[50vh]'}`}
+              style={{ width: isSmallScreen ? '100vh' : '100vh', maxWidth: '100%', height: isSmallScreen ? '40%' : '100vh' }}
             />
           </LocalizationProvider>
-          <div className='h-6'></div>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <TimeField
-              label="Heure Fin rendez-vous"
-              value={value}
-              onChange={(newValue) => setValue(newValue)}
-              format="HH:mm"
-            />
-          </LocalizationProvider>
+          <input
+            type="text"
+            value={selectedDate.format('YYYY-MM-DD')}
+            className={` sm:my-0 mx-2 border-2 border-gray-200 rounded-xl px-4 py-2 my-4 ${isSmallScreen ? 'w-[90%] ' : 'w-[80%]  ml-5'}`}
+            readOnly
+          />
         </div>
+        <div className={`flex justify-center ${isSmallScreen ? ' mt-5' : 'flex-col  mt-[20vh]'}`} >
+          <div className={` w-[100%]`}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <TimeField
+                label="Heure Debut rendez-vous"
+                value={value1}
+                onChange={(newValue) => setValue1(newValue)}
+                format="HH:mm"
+              />
+            </LocalizationProvider>
+            <div className='h-6'></div>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <TimeField
+                label="Heure Fin rendez-vous"
+                value={value}
+                onChange={(newValue) => setValue(newValue)}
+                format="HH:mm"
+              />
+            </LocalizationProvider>
+          </div>
         </div>
-    </div>
-    <div className={`flex justify-start ${isSmallScreen ? 'mt-[130vh]' : 'mt-[74vh] ml-[45%]'}`}>
-      <button
-        className={`bg-[#FFB1A6] rounded-2xl mr-10 ${isSmallScreen ? 'w-[20vh] ' : 'w-[16vh]'} h-8`} onClick={handleConfirmClick}>
-        Confirmer
-      </button>
-        <button className={`border border-black rounded-2xl ${isSmallScreen ? 'w-[20vh]' : 'w-[16vh]'} h-8 z-10`}  onClick={onAnnulerClick}>Annuler</button>
+      </div>
+      <div className={`flex justify-start ${isSmallScreen ? 'mt-[130vh]' : 'mt-[74vh] ml-[45%]'}`}>
+        <button
+          className={`bg-[#FFB1A6] rounded-2xl mr-10 ${isSmallScreen ? 'w-[20vh] ' : 'w-[16vh]'} h-8`} onClick={handleConfirmClick}>
+          Confirmer
+        </button>
+        <button className={`border border-black rounded-2xl ${isSmallScreen ? 'w-[20vh]' : 'w-[16vh]'} h-8 z-10`} onClick={onAnnulerClick}>Annuler</button>
       </div>
     </div>
   );
-
 };
-
 
 export default RendezVous;
