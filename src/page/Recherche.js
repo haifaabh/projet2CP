b2a901c9ac1c018a9daf255294bc129a9293b0b7
Dina <102818956+Dinouch@users.noreply.github.com>
2023-06-06 23:55:53 +0100

    Add files via upload

--- src/page/Recherche.js
+++ src/page/Recherche.js
@@ -1,53 +1,58 @@
-import React, { useEffect  } from 'react'
+import React, { useEffect } from 'react';
 // import JSONDATA from './MOCK_DATA.json'
-import './style.css'
-import {useState} from 'react' 
+import './style.css';
+import { useState } from 'react';
 import axios from 'axios';
 import InfoBox from '../Component/Navigation/InfoCreche';
 
 function Recherche({ localisation, handleLocalisationChange, creches, handleSearch }) {
-    const [nomC,setNomC]=useState('');
-    useEffect(() => {
-      if (localisation) {
-        handleSearch(localisation);
-      }
-    }, [localisation]);
-  
-    const handleToggleFavorite = async (crecheId) => {
-        try {
-          const response = await axios.post(`api/parent/favoris/${crecheId}`);
-          console.log(response.data);
-        } catch (error) {
-          console.log('yadraaa');
-          console.error(error);
-        }
-    };
+  const [nomC, setNomC] = useState('');
 
-    return (
-      <div className="recherche">
-        <div className="search-input">
-          <input
+  useEffect(() => {
+    // Effectuer la recherche lorsque la localisation change
+    if (localisation) {
+      handleSearch(localisation);
+    }
+  }, [localisation]);
+
+  const handleToggleFavorite = async (crecheId) => {
+    try {
+      // Effectuer la requête pour ajouter/supprimer des favoris
+      const response = await axios.post(`api/parent/favoris/${crecheId}`);
+      console.log(response.data);
+    } catch (error) {
+      console.log('yadraaa');
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="recherche">
+      <div className="search-input">
+        <input
           type="text"
           placeholder="Refaire une recherche..."
           value={localisation}
           onChange={handleLocalisationChange}
         />
-        
+
         <span className="icon-rech">
           <i className="fa-solid fa-magnifying-glass" style={{ color: '#000000' }}></i>
         </span>
       </div>
-      
-      {creches.length > 0 && 
+
+      {creches.length > 0 &&
         creches.map((creche, index) => {
           return (
             <div className="user" key={index}>
-         <InfoBox
+              {/* Afficher les informations de la crèche */}
+              <InfoBox
                 location={creche.nom}
                 days={creche.jours_accueil.join(', ')}
                 phone={creche.tél}
                 onFicheOpen={() => setNomC(creche.nom)}
               />
+              {/* Bouton pour ajouter/supprimer des favoris */}
               <button onClick={() => handleToggleFavorite(creche._id)}>Toggle Favorite</button>
             </div>
           );
@@ -57,8 +62,3 @@ function Recherche({ localisation, handleLocalisationChange, creches, handleSear
 }
 
 export default Recherche;
-
-
-
-
-
